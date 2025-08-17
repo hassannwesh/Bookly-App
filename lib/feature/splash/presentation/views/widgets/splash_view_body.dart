ddf1d7f529b000f06dce2e3006bedb40c010d168
hassannwesh <hassannweshy12345@gmail.com>
2025-08-17 18:56:19 +0300

    splash_view ui done

--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -6,7 +7,11 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'constent.dart';
+
 void main() {
   runApp(const BookApp());
 }
@@ -12,8 +14,9 @@ class BookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       home: SplashView(),
     );
   }
 }
-
