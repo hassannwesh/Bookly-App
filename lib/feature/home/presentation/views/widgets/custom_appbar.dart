094c36315dbb644da92c6596ed1de8a149606c3d
hassannwesh <hassannweshy12345@gmail.com>
2025-08-19 15:58:28 +0300

    create home view body and custom appbar

---
+++ lib/feature/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBre extends StatelessWidget {
+  const CustomAppBre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18,vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo, height: 18,),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 24),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/feature/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_appbar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBre(),
+      ],
+    );
+  }
+}
+
+
