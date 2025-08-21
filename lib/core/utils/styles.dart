6eacdac1a883429575b97ad3298ba5566d33f16c
hassannwesh <hassannweshy12345@gmail.com>
2025-08-21 08:42:53 +0300

    create styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,27 @@
+import 'package:bookly_app/constent.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-    fontSize: 20,
+  static const testStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const testStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const testStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const testStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+  static const testStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
   );
 }
