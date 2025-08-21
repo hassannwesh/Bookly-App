ae2c2d91bcddf507d3732476b29d19b44c57d111
hassannwesh <hassannweshy12345@gmail.com>
2025-08-21 09:39:18 +0300

    best seller p2

--- lib/constent.dart
+++ lib/constent.dart
@@ -2,4 +2,4 @@ import 'dart:ui';
 
 const  kPrimaryColor= Color(0xff100B20);
 const  kTranstionDuration=Duration(microseconds:250 );
-const  kGtSectraFine='Gt Sectra Fine';
+const  kGtSectraFine='GTSectraFine';
--- lib/feature/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/feature/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../../../../core/utils/assets.dart';
@@ -10,21 +11,30 @@ class BestSellerListViewItem extends StatelessWidget {
     return SizedBox(
       height: 125,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        AspectRatio(
-        //for different size device
-        aspectRatio: 2.5 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              image: AssetImage(AssetsData.testImage),
-              fit: BoxFit.fill,
+          AspectRatio(
+            //for different size device
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-
+          const SizedBox(width: 30),
+          SizedBox(
+            width: 200,
+            child: Text('Hary Potter and the Gold of Fine',
+                style: Styles.testStyle20,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
         ],
       ),
     );
