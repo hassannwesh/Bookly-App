3219ed6f7038c5a4417effda0727e6abec42901b
hassannwesh <hassannweshy12345@gmail.com>
2025-08-23 16:12:04 +0300

    add title and supTitle in details view

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/feature/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/feature/home/presentation/views/widgets/custom_book_details_appBar.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -8,16 +10,28 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width=MediaQuery.of(context).size.width;
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           CustomBookDetailsAppbar(),
           Padding(
-            padding:  EdgeInsets.symmetric(horizontal: width*0.19),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.25),
             child: CustomImageBook(),
           ),
+          const SizedBox(height: 43),
+          Text('The Jungle Bool', style: Styles.testStyle30),
+          const SizedBox(height: 6),
+          Opacity(
+            opacity: 0.5,
+            child: Text('Rudyard Kipling', style: Styles.testStyle18.copyWith(
+              fontStyle: FontStyle.italic
+            )),
+          ),
+          const SizedBox(height: 30,),
+
         ],
       ),
     );
