4128d9e1c85eb6261568c0ab0895c17356c7af37
hassannwesh <hassannweshy12345@gmail.com>
2025-08-24 15:44:46 +0300

    add search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/feature/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
+import 'package:bookly_app/feature/search/presentation/views/search_view.dart';
 import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
@@ -7,12 +8,19 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeview';
   static const kBookDetailsView = '/bookdetailsview';
+  static const kSearchView = '/searchView';
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return const SplashView();
+        },
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const SearchView();
         },
       ),
       GoRoute(
