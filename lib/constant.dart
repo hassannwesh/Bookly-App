205d669f87892cc127cf26bb8def3f599b275df4
hassannwesh <hassannweshy12345@gmail.com>
2025-08-27 10:12:39 +0300

    create book model

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,7 @@ import 'dart:ui';
 const  kPrimaryColor= Color(0xff100B20);
 const  kTranstionDuration=Duration(microseconds:250 );
 const  kGtSectraFine='GTSectraFine';
+
+
+//flutter emulators
+//flutter emulators --launch Pixel_7
