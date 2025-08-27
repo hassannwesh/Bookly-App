a973bad92b511dafe0d8aade9baf9473533c09f8
hassannwesh <hassannweshy12345@gmail.com>
2025-08-27 15:36:43 +0300

    create home repo methods

--- lib/feature/home/data/models/repos/home_repo.dart
+++ lib/feature/home/data/models/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  // either is used to handel errors form adrtz package
+Future<Either<Failures,List<BookModel>>> fetchFeaturedBooks();
+Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks();
 }
