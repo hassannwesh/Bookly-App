dc616e4886991d680c6d763582af21585dd02c0b
hassannwesh <hassannweshy12345@gmail.com>
2025-09-03 19:17:12 +0300

    feach similar books

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,8 +1,14 @@
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/feature/home/data/models/repos/home_repo.dart';
+import 'package:bookly_app/feature/home/data/models/repos/home_repo_impl.dart';
+import 'package:bookly_app/feature/home/presentation/manger/similar_books/similar_books_cubit.dart';
 import 'package:bookly_app/feature/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
 import 'package:bookly_app/feature/search/presentation/views/search_view.dart';
 import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -32,7 +38,11 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (BuildContext context, GoRouterState state) {
-          return const BookDetailsView();
+          return BlocProvider(
+              create: (context) =>SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+            child: BookDetailsView(
+              bookModel:state.extra as BookModel,
+            ));
         },
       ),
     ],
---
+++ lib/feature/home/presentation/manger/similar_books/similar_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/feature/home/data/models/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchSimilarBooks({required String category}) async {
+    emit(SimilarBooksLoding());
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+    result.fold(
+      (failure) {
+        emit(SimilarBooksFaliure(failure.errMessage));
+      },
+      (books) {
+        emit(SimilarBooksSuccess(books));
+      },
+    );
+  }
+
+}
